daa23f2c8e5893b1f0435d2419e3c153a660e3e5
liesbeto <lies.ooghe@gmail.com>
2025-01-22 14:23:52 +0100

    colourmap problem fixed
    vmin and vmax implemented so that when minimum and maximum value of crime change the colourmap doesn't

--- first_try.py
+++ first_try.py
@@ -8,18 +8,18 @@ class District():
         self.j = j              # column
         self.neighbours = []
         self.crimes = [crimes0]
-    
+
     def find_neigbours(self, map):
         for i in [self.i-1, self.i+1]:
             if i < (map.i_len) and i >= 0:
                 self.neighbours.append(map.districts[f"{i},{self.j}"])
-        
+
         for j in [self.j-1, self.j+1]:
             if j < (map.j_len) and j >= 0:
                 self.neighbours.append(map.districts[f"{self.i},{j}"])
-        
+
         return self.neighbours
-    
+
     def next_timestep(self, k):
         crime = self.crimes[-1]
         for neighbour in self.neighbours:
@@ -39,8 +39,7 @@ class Map():
 def timesteps(map, n):
     for time in range(n):
         for item in map.districts:
-            map.districts[item].next_timestep(0.4)
-        
+            map.districts[item].next_timestep(0.9)
 
         # plots
         i_list, j_list, colours = [], [], []
@@ -51,15 +50,17 @@ def timesteps(map, n):
             j_list.append(j)
             colours.append(map.districts[item].crimes[-1])
 
-        plt.scatter(j_list, i_list, s=100, c=colours, marker="s",)
+        plt.scatter(j_list, i_list, s=5, c=colours, marker="s", cmap="binary", vmin=0, vmax=1)
         plt.title(time)
         plt.axis("square")
+        plt.axis('off')
+        plt.colorbar()
         plt.draw()           # update grafiek
-        plt.pause(0.5)
+        plt.pause(0.1)
         plt.clf()            # clear grafiek
     plt.show()
 
-map = Map(20, 20)
+map = Map(100, 100)
 for item in map.districts:
     map.districts[item].find_neigbours(map)
 
